8169e069fafd3fbb9e0d341816bf93350319cc06
Martin Čorovčák <martino.coro@gmail.com>
2024-05-17 16:11:16 +0200

    Citations service

---
+++ common/datasets/serializers/citations/csl/__init__.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""CSL JSON and  citation string serializers for Invenio RDM Records."""
+
+import re
+
+from citeproc import (
+    Citation,
+    CitationItem,
+    CitationStylesBibliography,
+    CitationStylesStyle,
+    formatter,
+)
+from citeproc.source.json import CiteProcJSON
+from citeproc_styles import get_style_filepath
+from citeproc_styles.errors import StyleNotFoundError
+from flask import current_app
+from flask_resources import BaseListSchema, MarshmallowSerializer
+from flask_resources.serializers import JSONSerializer
+from webargs import fields
+
+from .schema import CSLJSONSchema
+
+
+class CSLJSONSerializer(MarshmallowSerializer):
+    """Marshmallow based CSL JSON serializer for records."""
+
+    def __init__(self, **options):
+        """Constructor."""
+        super().__init__(
+            format_serializer_cls=JSONSerializer,
+            object_schema_cls=CSLJSONSchema,
+            list_schema_cls=BaseListSchema,
+            **options,
+        )
+
+
+def get_citation_string(json, id, style, locale):
+    """Get the citation string from CiteProc library."""
+
+    def _clean_result(text):
+        """Remove double spaces, punctuation."""
+        text = re.sub(r"\s\s+", " ", text)
+        text = re.sub(r"\.\.+", ".", text)
+        return text
+
+    source = CiteProcJSON([json])
+    citation_style = CitationStylesStyle(validate=False, style=style, locale=locale)
+    bib = CitationStylesBibliography(citation_style, source, formatter.plain)
+    citation = Citation([CitationItem(id)])
+    bib.register(citation)
+
+    return _clean_result(str(bib.bibliography()[0]))
+
+
+def get_style_location(style):
+    """Return the path to the CSL style if exists or throw."""
+    try:
+        return get_style_filepath(style.lower())
+    except StyleNotFoundError as ex:
+        current_app.logger.warning(f"CSL style {style} not found.")
+        raise ex
+
+
+class StringCitationSerializer(MarshmallowSerializer):
+    """CSL Citation Formatter serializer for records.
+
+    In order to produce a formatted citation of a record through citeproc-py,
+    we need a CSL-JSON serialized version of it.
+    """
+
+    _default_style = "harvard1"
+    """The `citeproc-py` library supports by default the 'harvard1' style."""
+
+    _default_locale = "en-US"
+    """The `citeproc-py` library supports by default the 'harvard1' style."""
+
+    _user_args = {
+        "style": fields.Str(load_default=_default_style),
+        "locale": fields.Str(load_default=_default_locale),
+    }
+    """Arguments for the webargs parser."""
+
+    _valid_formats = ("csl", "bibtex")
+    """Supported formats by citeproc-py."""
+
+    def __init__(self, url_args_retriever, **options):
+        """Constructor.
+
+        :param url_args_retriever: callable func or object that return the
+                                   style and locale URL args
+        """
+        super().__init__(
+            format_serializer_cls=JSONSerializer,
+            object_schema_cls=CSLJSONSchema,
+            list_schema_cls=BaseListSchema,
+            **options,
+        )
+        self.url_args_retriever = url_args_retriever
+
+    def serialize_object(self, record):
+        """Serialize a single record.
+
+        :param record: Record instance.
+        """
+        style, locale = (
+            self.url_args_retriever()
+            if callable(self.url_args_retriever)
+            else self.url_args_retriever
+        )
+
+        # set defaults if params are not provided
+        style = style or self._default_style
+        locale = locale or self._default_locale
+
+        style_filepath = get_style_location(style)
+
+        return get_citation_string(
+            self.dump_obj(record), record["id"], style_filepath, locale
+        )
+
+    def serialize_object_list(self, records):
+        """Serialize a list of records.
+
+        :param records: List of records instance.
+        """
+        return "\n".join(
+            [self.serialize_object(rec) for rec in records["hits"]["hits"]]
+        )
---
+++ common/datasets/serializers/citations/csl/schema.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""CSL based Schema for Invenio RDM Records."""
+
+from edtf import parse_edtf
+from edtf.parser.edtf_exceptions import EDTFParseException
+from edtf.parser.parser_classes import Date, Interval
+from invenio_access.permissions import system_identity
+from invenio_vocabularies.proxies import current_service as vocabulary_service
+from marshmallow import Schema, fields, missing
+from marshmallow_utils.fields import SanitizedUnicode
+from oarepo_runtime.services.schema.i18n_ui import (
+    I18nStrUIField,
+    MultilingualLocalizedUIField,
+)
+from gettext import gettext as _
+
+
+class CSLCreatorSchema(Schema):
+    """Creator/contributor common schema."""
+
+    literal = fields.Str(attribute="name")
+
+
+def add_if_not_none(year, month, day):
+    """Adds year, month a day to a list if each are not None."""
+    _list = []
+    _list.append(year) if year else None
+    _list.append(month) if month else None
+    _list.append(day) if day else None
+    return _list
+
+
+class CSLJSONSchema(Schema):
+    """CSL Marshmallow Schema."""
+
+    id_ = SanitizedUnicode(data_key="id", attribute="id")
+    type_ = fields.Method("get_type", data_key="type")
+    title = SanitizedUnicode(attribute="metadata.titles.0.title")
+    abstract = MultilingualLocalizedUIField(I18nStrUIField(value_field="description"))
+    author = fields.List(fields.Nested(CSLCreatorSchema()), attribute="metadata.creators")
+    issued = fields.Method("get_issued")
+    language = fields.Method("get_language")
+    version = SanitizedUnicode(attribute="metadata.version")
+    # publisher = SanitizedUnicode(attribute="metadata.publisher", default=_("UCT Prague"))
+
+    def _read_resource_type(self, id_):
+        """Retrieve resource type record using service."""
+        rec = vocabulary_service.read(system_identity, ("resource-types", id_))
+        return rec._record
+
+    def get_type(self, obj):
+        """Get resource type."""
+        resource_type = obj["metadata"].get(
+            "types", [{"resourceTypeGeneral": "thesis"}] # article is CSL "Other"
+        )[0]
+
+        resource_type_record = self._read_resource_type(resource_type["resourceTypeGeneral"])
+        props = resource_type_record["props"]
+        return props.get("csl", "thesis")  # article is CSL "Other"
+
+    def get_issued(self, obj):
+        """Get issued dates."""
+        try:
+            filtered = filter(lambda o: o.get("dateType").lower() == "issued", obj["metadata"].get("dates"))[0]
+            parsed = parse_edtf(filtered.get("date"))
+        except EDTFParseException:
+            return missing
+
+        if isinstance(parsed, Date):
+            parts = add_if_not_none(parsed.year, parsed.month, parsed.day)
+            return {"date-parts": [parts]}
+        elif isinstance(parsed, Interval):
+            d1 = parsed.lower
+            d2 = parsed.upper
+            return {
+                "date-parts": [
+                    add_if_not_none(d1.year, d1.month, d1.day),
+                    add_if_not_none(d2.year, d2.month, d2.day),
+                ]
+            }
+        else:
+            return missing
+
+    def get_language(self, obj):
+        """Get language."""
+        metadata = obj["metadata"]
+        language = metadata.get("language")
+
+        return language if language else missing
