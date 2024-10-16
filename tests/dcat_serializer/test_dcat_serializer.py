e656f8a1bda8108e3708606baef64e7c760d11f2
Dominik Prager <dominik.prager@cesnet.cz>
2024-10-16 16:03:16 +0200

    fix: refactor DCAT serializer + headers

--- tests/dcat_serializer/test_dcat_serializer.py
+++ tests/dcat_serializer/test_dcat_serializer.py
@@ -13,26 +13,51 @@
 # Register the serializer to UI exports
 # Write API connector for content negotiation
 
-import jsonschema
 import json
-import argparse
+import jsonschema
+from common.datasets.serializers.dcat import DCATAPSerializer
 
 def load_json(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
 
-# now use jsonschema to validate the JSON file against the schema
-def validate_json(json_file, schema_file):
-    json_data = load_json(json_file)
-    schema_data = load_json(schema_file)
+def validate_json(json_data, schema_data):
     jsonschema.validate(json_data, schema_data)
 
-def main(json_file, schema_file):
-    validate_json(json_file, schema_file)
+def normalize_json(json_data):
+    """Consistent JSON formatting for comparison."""
+    return json.dumps(json_data, indent=2, sort_keys=True)
+
+def test_dcatap_serializer():
+    input_file = './input_file.json'
+    expected_output_file = './expected_output_file.json'
+    
+    json_data = load_json(input_file)
+    expected_output_data = load_json(expected_output_file)
+    
+    serializer = DCATAPSerializer()
+    output_data = serializer.serialize_object(json_data)
+    
+    # dict
+    if isinstance(output_data, str):
+        output_data = json.loads(output_data)
+    
+    output_data_str = normalize_json(output_data)
+    expected_output_data_str = normalize_json(expected_output_data)
+    
+    assert output_data_str == expected_output_data_str
+    return output_data
+
+def test_dcatap_schema_validation():
+    output_data = test_dcatap_serializer()
+    schema_file = './datacite-4.3.json'
+    
+    schema_data = load_json(schema_file)
+    
+    metadata = output_data.get('metadata', {})
+    
+    assert validate_json(metadata, schema_data) is None
+    
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Validate a JSON file against a schema.")
-    parser.add_argument("json_file", help="Path to the JSON file.")
-    parser.add_argument("schema_file", help="Path to the schema file.")
-    args = parser.parse_args()
-    main(args.json_file, args.schema_file)
+    test_dcatap_schema_validation()
