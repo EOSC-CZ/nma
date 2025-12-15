8f02ad23800cfd12a18e07315486545f6101232f
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2025-12-15 08:17:36 +0100

    Miroslavsimek/sta 21 transformer pro catch all (#67)
    * Working on catchall reader & transformer
    
    * Working on catchall reader & transformer
    
    * working on transformer
    
    * Working on catch-all harvest
    
    * Harvesting changes
    
    * fix: invenio.cfg, deps upgrade
    
    * persistent identifier format
    
    * Resolved issues - round 1

--- riv/cli.py
+++ riv/cli.py
@@ -37,3 +37,15 @@ def check_availability(eager=False):
     else:
         check_availability_task.delay()
         click.secho("Availability check task sent to Celery queue...", fg="yellow")
+
+
+@riv.command("temporary-harvest-catch-all")
+@with_appcontext
+def temporary_harvest_catch_all():
+    """Temporary command to harvest catch-all OAI-PMH repository before jobs have cli."""
+    from oarepo_oaipmh_harvester.tasks import harvest_oaipmh_records
+
+    # harvest all records from the catch-all repository
+    # not using .delay here as we want to run it synchronously
+    # for testing purposes
+    harvest_oaipmh_records(harvester_id="catchall", batch_size=1)
