bf55e493f7da48fc05d16b22e90f5022ef8fe952
Martin Čorovčák <martino.coro@gmail.com>
2025-12-16 12:33:42 +0100

    feat: update .gitignore, add theme search endpoint, and enhance ResultsListItem component

--- ui/datasets/semantic-ui/js/datasets/search/ResultsListItem.jsx
+++ ui/datasets/semantic-ui/js/datasets/search/ResultsListItem.jsx
@@ -1,82 +1,150 @@
-import React from "react";
-import PropTypes from "prop-types";
+// This file is part of InvenioRDM
+// Copyright (C) 2022-2024 CERN.
+// Copyright (C) 2024 KTH Royal Institute of Technology.
+//
+// Invenio RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import { i18next } from "@translations/invenio_app_rdm/i18next";
 import _get from "lodash/get";
-import _join from "lodash/join";
-import { Grid, Item, Label } from "semantic-ui-react";
-import { i18next } from "@translations/i18next";
+import React, { Component } from "react";
+import { SearchItemCreators } from "@js/invenio_app_rdm/utils";
+import PropTypes from "prop-types";
+import { Item, Label, Icon } from "semantic-ui-react";
+import { CompactStats } from "@js/invenio_app_rdm/components/CompactStats";
+import { DisplayPartOfCommunities } from "@js/invenio_app_rdm/components/DisplayPartOfCommunities";
+import { withState } from "react-searchkit";
 
-export const ResultsListItem = ({ result, ...rest }) => {
-  const accessRights = _get(result, "ui.access_status", null);
-  const createdDate = _get(
-    result,
-    "ui.created_date_l10n_short",
-    "No creation date found."
-  );
-  const languages = _get(result, "metadata.languages", []);
-  const version = _get(result, "metadata.version", null);
-  const title = _get(result, "metadata.title", i18next.t("No title"));
-  return (
-    <Item key={result.id} data-testid="result-item">
-      <Item.Content>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column className="results-list item-main">
-              <div className="justify-space-between flex">
-                <Item.Header as="h2">
-                  <a href={result.links.self_html}>{title}</a>
-                </Item.Header>
-                <div className="item-access-rights">
-                  {result.state && (
-                    <Label title={result.state_timestamp}>{result.state}</Label>
-                  )}
-                  {accessRights && accessRights.id !== "open" && (
-                    <Label title={`${accessRights.description_l10n}`}>
-                      {accessRights.title_l10n}
-                    </Label>
-                  )}
-                </div>
+class RecordsResultsListItem extends Component {
+  render() {
+    const { result, key } = this.props;
+
+    const viewLink = _get(result, "links.self_html");
+    const accessStatusId = _get(result, "ui.access_status.id", "open");
+    const accessStatus = _get(result, "ui.access_status.title_l10n", "Open");
+    const accessStatusIcon = _get(result, "ui.access_status.icon", "unlock");
+    const createdDate = _get(
+      result,
+      "ui.created_date_l10n_long",
+      i18next.t("No creation date found.")
+    );
+
+    const creators = result.ui.creators.creators;
+
+    const descriptionStripped = _get(
+      result,
+      "ui.description_stripped",
+      i18next.t("No description")
+    );
+
+    const publicationDate = _get(
+      result,
+      "ui.publication_date_l10n_long",
+      i18next.t("No publication date found.")
+    );
+    const resourceType = _get(
+      result,
+      "ui.resource_type.title_l10n",
+      i18next.t("No resource type")
+    );
+    const subjects = _get(result, "ui.subjects", []);
+    const title = _get(result, "metadata.title", i18next.t("No title"));
+    const uniqueViews = _get(result, "stats.all_versions.unique_views", 0);
+    const uniqueDownloads = _get(result, "stats.all_versions.unique_downloads", 0);
+
+    const publishingInformation = _get(result, "ui.publishing_information.journal", "");
+
+    return (
+      <Item key={key ?? result.id}>
+          <Item.Content>
+            <Item.Extra className="labels-actions">
+              <Label horizontal size="small" className="primary theme-primary">
+                {publicationDate}
+              </Label>
+              <Label horizontal size="small" className="neutral">
+                {resourceType}
+              </Label>
+              <Label
+                horizontal
+                size="small"
+                className={`access-status ${accessStatusId}`}
+              >
+                {accessStatusIcon && <Icon name={accessStatusIcon} />}
+                {accessStatus}
+              </Label>
+            </Item.Extra>
+            <Item.Header as="h2" className="theme-primary-text">
+              <a href={viewLink}>{title}</a>
+            </Item.Header>
+            <Item className="creatibutors">
+              <SearchItemCreators creators={creators} othersLink={viewLink} />
+            </Item>
+            <Item.Description className="truncate-lines-2">
+              {descriptionStripped}
+            </Item.Description>
+
+            <Item.Extra>
+              {subjects.map((subject) => (
+                <Label key={subject.title_l10n} size="tiny">
+                  {subject.title_l10n}
+                </Label>
+              ))}
+
+              <div className="flex justify-space-between align-items-end">
+                <small>
+                  <DisplayPartOfCommunities communities={result.parent?.communities} />
+                  <p>
+                    {createdDate && (
+                      <>
+                        {i18next.t("Uploaded on {{uploadDate}}", {
+                          uploadDate: createdDate,
+                        })}
+                      </>
+                    )}
+                    {createdDate && publishingInformation && " | "}
+
+                    {publishingInformation && (
+                      <>
+                        {i18next.t("Published in: {{- publishInfo }}", {
+                          publishInfo: publishingInformation,
+                        })}
+                      </>
+                    )}
+                  </p>
+                </small>
+
+                <small>
+                  <CompactStats
+                    uniqueViews={uniqueViews}
+                    uniqueDownloads={uniqueDownloads}
+                  />
+                </small>
               </div>
-              <Item.Meta>
-                <Grid columns={1}>
-                  <Grid.Column>
-                    <Grid.Row className="ui separated">
-                      <span
-                        aria-label={i18next.t("Languages")}
-                        title={i18next.t("Languages")}
-                      >
-                        {_join(
-                          languages.map((l) => l.title),
-                          ", "
-                        )}
-                      </span>
-                    </Grid.Row>
-                  </Grid.Column>
-                </Grid>
-              </Item.Meta>
-              <Item.Extra>
-                <div>
-                  <small>
-                    <p>
-                      {createdDate && (
-                        <>
-                          {i18next.t("Uploaded on")} <span>{createdDate}</span>{" "}
-                          {version && `(${i18next.t("version")}: ${version})`}
-                        </>
-                      )}
-                    </p>
-                  </small>
-                </div>
-              </Item.Extra>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Item.Content>
-    </Item>
-  );
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+    );
+  }
+}
+
+RecordsResultsListItem.propTypes = {
+  currentQueryState: PropTypes.object,
+  result: PropTypes.object.isRequired,
+  key: PropTypes.string,
 };
 
-ResultsListItem.propTypes = {
-  result: PropTypes.object.isRequired,
+RecordsResultsListItem.defaultProps = {
+  key: null,
+  currentQueryState: null,
 };
+
+const ResultsListItem = withState(
+  ({ currentQueryState, result }) => (
+    <RecordsResultsListItem
+      currentQueryState={currentQueryState}
+      result={result}
+    />
+  )
+);
 
 export default ResultsListItem;
--- ui/datasets/semantic-ui/js/datasets/search/index.js
+++ ui/datasets/semantic-ui/js/datasets/search/index.js
@@ -2,21 +2,43 @@ import {
   parseSearchAppConfigs,
   createSearchAppsInit,
 } from "@js/oarepo_ui/search";
+
+import {
+  RDMCountComponent,
+  RDMEmptyResults,
+  RDMErrorComponent,
+  RDMRecordResultsGridItem,
+  RDMRecordSearchBarContainer,
+  RDMRecordMultipleSearchBarElement,
+  RDMToggleComponent,
+} from "@js/invenio_app_rdm/search/components";
+import {
+  ContribSearchAppFacets,
+  ContribBucketAggregationElement,
+  ContribBucketAggregationValuesElement,
+} from "@js/invenio_search_ui/components";
+import { parametrize } from "react-overridable";
+
 import ResultsListItem from "./ResultsListItem";
 
-/** NOTE: This reads configs for any search app present on a page
- *   In HTML/Jinja, a single search app instance is typically represented
-@@ -14,13 +15,13 @@ import {
-/** NOTE: To customize components in a specific search app instance,
- *   you need to obtain its `overridableIdPrefix` from the corresponding config first
- */
 const [{ overridableIdPrefix }] = parseSearchAppConfigs();
 
+export const RDMRecordSearchBarContainerWithConfig = parametrize(RDMRecordSearchBarContainer, {
+  appName: overridableIdPrefix,
+});
+
 export const componentOverrides = {
-  /** NOTE: Then you can then replace any existing search ui
-   * component with your own implementation, e.g.:
-   */
+  [`${overridableIdPrefix}.BucketAggregation.element`]: ContribBucketAggregationElement,
+  [`${overridableIdPrefix}.BucketAggregationValues.element`]: ContribBucketAggregationValuesElement,
+  [`${overridableIdPrefix}.ResultsGrid.item`]: RDMRecordResultsGridItem,
+  [`${overridableIdPrefix}.EmptyResults.element`]: RDMEmptyResults,
   [`${overridableIdPrefix}.ResultsList.item`]: ResultsListItem,
+  [`${overridableIdPrefix}.SearchApp.facets`]: ContribSearchAppFacets,
+  [`${overridableIdPrefix}.SearchApp.searchbarContainer`]: RDMRecordSearchBarContainerWithConfig,
+  [`${overridableIdPrefix}.SearchBar.element`]: RDMRecordMultipleSearchBarElement,
+  [`${overridableIdPrefix}.Count.element`]: RDMCountComponent,
+  [`${overridableIdPrefix}.Error.element`]: RDMErrorComponent,
+  [`${overridableIdPrefix}.SearchFilters.Toggle.element`]: RDMToggleComponent,
 };
 
 createSearchAppsInit({ componentOverrides });
