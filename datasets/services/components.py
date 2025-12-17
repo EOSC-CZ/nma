b0138617aa4d9d2559ab3c73a7d4c2f9a80bdd4c
Ondřej Ruml <33997345+kyodai0000@users.noreply.github.com>
2025-12-17 16:36:09 +0100

    feat: Add fields expiration and access_granted to editor dict (#97)
    * feat: Add fields expiration and access_granted to editor dict
    
    * chore: deal with possible unset values on User
    
    * chore: simplify editors update

--- datasets/services/components.py
+++ datasets/services/components.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from flask_login import current_user
 from flask_principal import Identity
+from invenio_accounts.models import User
 from invenio_drafts_resources.records import Record
 from invenio_records_resources.services.records.components import ServiceComponent
 
@@ -27,37 +28,54 @@ class UpdateMetadataComponent(ServiceComponent):
         setattr(record, self.field, data.get(self.field, {}))
 
 
+def _get_editor_dict_for_user(user: User | None = None):
+    if user is None:
+        user = current_user
+    if user is None:
+        return {"id": "Anonymous", "full_name": "Anonymous", "affiliations": ""}
+    user_profile =  getattr(user, "user_profile", {})
+    user_name = user_profile.get("full_name", "")
+    if not user_name:
+        user_email = getattr(user, "email", "")
+        user_name = user_email.split("@")[0][:-3] + "***"
+    return {"id": str(user.id), "full_name": user_name, "affiliations": user_profile.get("affiliations", "")}
+
+
+def _update_editors_field_for_user(record: Record, editor: dict) -> dict:
+    editors = record.setdefault("editors", [])
+    for obj in editors:
+        if obj["id"] == editor["id"]:
+            obj["full_name"] = editor["full_name"]
+            obj["affiliations"] = editor["affiliations"]
+            return obj
+    else:
+        editors.append(editor)
+    return editor
+
+
+def register_editor(record: Record, user: User | None = None) -> dict:
+    editor = _get_editor_dict_for_user(user)
+    return _update_editors_field_for_user(record, editor)
+
+
 class UpdateEditorsComponent(ServiceComponent):
     """Modified by component."""
     field = "editors"
 
-    def _get_editor(self):
-        return {"id": str(current_user.id), "full_name": current_user.user_profile.get("full_name", "")}
-
-    def _update_editors(self, editors: list):
-        editor = self._get_editor()
-        for i, obj in enumerate(editors):
-            if obj["id"] == editor["id"]:
-                editors[i] = editor
-                break
-        return editors
-
     def create(self, identity: Identity, data: dict[str, Any] = None, record: Record = None, **kwargs: Any) -> None:
-        editor = self._get_editor()
-        record["editors"] = [editor]
+        register_editor(record)
 
     def update(self, identity: Identity, data: dict[str, Any] = None, record: Record = None, **kwargs: Any) -> None:
-        record["editors"] = self._update_editors(record.get("editors", []))
+        register_editor(record)
 
     def publish(self, identity, draft=None, record=None, **kwargs):
         """Update draft metadata."""
-        editor = self._get_editor()
-        record["editors"] = [editor]
+        register_editor(record)
 
     def edit(self, identity, draft=None, record=None, **kwargs):
         """Update draft metadata."""
-        record["editors"] = self._update_editors(record.get("editors", []))
+        register_editor(record)
 
     def new_version(self, identity, draft=None, record=None, **kwargs):
         """Update draft metadata."""
-        record["editors"] = self._update_editors(record.get("editors", []))
+        register_editor(record)
