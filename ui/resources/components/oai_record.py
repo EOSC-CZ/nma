8b5383a7404d389a780d3de07c6fe8c6f0789fb1
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2026-01-05 07:53:02 +0100

    storing oai_record directly in render_kwargs

--- ui/resources/components/oai_record.py
+++ ui/resources/components/oai_record.py
@@ -5,10 +5,12 @@
 
 from __future__ import annotations
 
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
+
 from invenio_db import db
 from oarepo_oaipmh_harvester.oai_record.models import OAIHarvestedRecord
 from oarepo_ui.resources.components import UIResourceComponent
+
 if TYPE_CHECKING:
     from flask_principal import Identity
     from invenio_records_resources.services.records.results import RecordItem
@@ -18,20 +20,23 @@ class OAIRecordComponent(UIResourceComponent):
     """Pass RDM vocabulary fixtures to form config."""
 
     def before_ui_detail(
-            self,
-            *,
-            api_record: RecordItem,
-            record: dict,
-            identity: Identity,
-            ui_links: dict,
-            extra_context: dict,
-            **kwargs: Any,
+        self,
+        *,
+        api_record: RecordItem,
+        record: dict,
+        identity: Identity,
+        ui_links: dict,
+        render_kwargs: dict,
+        **kwargs: Any,
     ) -> None:
         """
         Extends the UI detail context with OAI-PMH information.
         If the record was harvested via OAI-PMH, the corresponding
         OAIHarvestedRecord is loaded. Otherwise, the value is set to None.
         """
-        harvested_record = db.session.query(OAIHarvestedRecord).filter_by(record_pid=record["id"]).first()
-        extra_context["oai_record"] = harvested_record
-
+        harvested_record = (
+            db.session.query(OAIHarvestedRecord)
+            .filter_by(record_pid=record["id"])
+            .first()
+        )
+        render_kwargs["oai_record"] = harvested_record
