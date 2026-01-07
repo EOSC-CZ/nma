4fcb419bef8b4f929e40ea7867104f80910a2b07
Ronald Krist <34395689+SilvyPuzzlewell@users.noreply.github.com>
2026-01-07 15:43:24 +0100

    Grant notification (#162)
    * do not add edit grant if record registered with no problems
    
    * record creation without metadata retrieval assigns edit grant;
    cosmetic issues
    
    * removed unused imports
    
    * savepoint
    
    * savepoint
    
    * cosmetics
    
    * reverted uv.lock
    
    * grant-user-access.create.jinja plain body
    
    * grant-user-access.create.jinja correct template
    
    * Expiration hardcoded in template

---
+++ datasets/records/resolvers.py
@@ -0,0 +1,9 @@
+from invenio_records_resources.references.entity_resolvers.results import ServiceResultResolver
+
+class RecordByPIDServiceResultResolver(ServiceResultResolver):
+    """Resolver for rdm record result items."""
+
+    def _reference_entity(self, entity):
+        """Create a reference dict for the given result item."""
+        pid = entity.id if isinstance(entity, self.item_cls) else entity.pid.pid_value
+        return {self.type_key: str(pid)}
