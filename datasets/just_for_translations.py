505f0713362fbf0d3ee128be40c37912707950d3
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2026-01-07 10:34:57 +0100

    Extra facets (#160)

--- datasets/just_for_translations.py
+++ datasets/just_for_translations.py
@@ -2,3 +2,5 @@ from invenio_i18n import lazy_gettext as _
 
 # TODO: this should be handled in a better way
 _("metadata/publisher.label")
+_("metadata/resource_type.label")
+_("metadata/languages.label")
