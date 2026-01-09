d16574c2de2453300301ebd879ed45b75723a0ce
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2026-01-09 08:41:59 +0100

    docstrings

--- riv/resolvers/base.py
+++ riv/resolvers/base.py
@@ -81,6 +81,8 @@ class UnsupportedPIDError(Exception):
         super().__init__(f"Unsupported identifier '{identifier}'.")
 
 class PIDProcessingError(Exception):
+    """Raised when an error occurs while processing a persistent identifier."""
+    
     def __init__(self, identifier: str):
         self.identifier = identifier
         super().__init__(f"Error while processing identifier '{identifier}'.")
