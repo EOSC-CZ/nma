5c3b02a40c041865c59fee09d266601f563b6492
Ronald Krist <34395689+SilvyPuzzlewell@users.noreply.github.com>
2026-01-19 15:06:05 +0100

    Krist/sta 127 fix search crashing on regular longer query (#204)
    * using aggregated fields for search
    
    * some field selection fixes
    
    * analysers on boosted fields

--- datasets/model.py
+++ datasets/model.py
@@ -22,7 +22,7 @@ from oarepo_model.customizations import (
     PatchIndexPropertyMapping,
     PatchIndexSettings,
     PrependMixin,
-    ReplaceBaseClass,
+    ReplaceBaseClass, PatchIndexMapping, SetDefaultSearchFields,
 )
 from oarepo_model.customizations.high_level.add_link import AddLink
 from oarepo_model.datatypes.registry import from_yaml
@@ -138,6 +138,46 @@ class OverriddenRouteResourceConfigMixin:
         return updated_routes
 
 
+COPY_TO_MAPPINGS = [
+    # boost_10 - Primary identifiers (highest weight)
+    ("metadata.title", 10),
+    ("metadata.persistent_url", 10),
+    ("id", 10),
+
+    # boost_5 - Important searchable content
+    ("metadata.additional_titles.title", 5),
+    ("metadata.description", 5),
+
+    ("metadata.creators.person_or_org.name", 5),
+    # Author names
+    # boost_1 - Supplementary content
+    ("metadata.additional_descriptions.description", 1),
+    ("metadata.contributors.person_or_org.name", 1),
+    # Contributor names
+    ("metadata.publisher", 1),
+    # Publisher
+    ("metadata.funding.funder.name", 1),
+    # Funder names
+    ("metadata.locations.features.place", 1),
+    # Place names
+    ("metadata.references.reference", 1)
+    ]
+
+copy_to_mappings = [PatchIndexPropertyMapping(c[0], {"copy_to": f"boost_{c[1]}"}) for c in COPY_TO_MAPPINGS]
+analyzer_fields = {"fields": {
+    # using both means that queries that match both ascii and non-ascii
+    # versions are ranked higher (if query is Novák, records with Novák
+    # will have better ranking than Novak and both will be found),
+    # but if user searches for Novak Novák will still match with
+    # lower ranking than Novak
+    "_search": {"type": "text", "analyzer": "lowercase_analyzer"},
+    "_ascii_search": {
+        "type": "text",
+        "analyzer": "asciifolded_lowercase_analyzer",
+    },
+}
+}
+
 datasets_model = model(
     "datasets",
     version="1.0.0",
@@ -217,15 +257,15 @@ datasets_model = model(
             {
                 "analysis": {
                     # lowercase splits on whitespaces and performs lowercasing
-                    "tokenizer": {"people_tokenizer": {"type": "lowercase"}},
+                    "tokenizer": {"lowercase_tokenizer": {"type": "lowercase"}},
                     "analyzer": {
-                        "people_analyzer": {
+                        "lowercase_analyzer": {
                             "type": "custom",
-                            "tokenizer": "people_tokenizer",
+                            "tokenizer": "lowercase_tokenizer",
                         },
-                        "asciifolded_people_analyzer": {
+                        "asciifolded_lowercase_analyzer": {
                             "type": "custom",
-                            "tokenizer": "people_tokenizer",
+                            "tokenizer": "lowercase_tokenizer",
                             # additionally removes diacritics
                             "filter": ["asciifolding"],
                         },
@@ -233,36 +273,19 @@ datasets_model = model(
                 }
             }
         ),
-        # add multi-field mappings to people names for improved searching/suggesting
-        PatchIndexPropertyMapping(
-            "metadata.creators.person_or_org.name",
+        PatchIndexMapping(
             {
-                "fields": {
-                    # using both means that queries that match both ascii and non-ascii
-                    # versions are ranked higher (if query is Novák, records with Novák
-                    # will have better ranking than Novak and both will be found),
-                    # but if user searches for Novak Novák will still match with
-                    # lower ranking than Novak
-                    "_search": {"type": "text", "analyzer": "people_analyzer"},
-                    "_ascii_search": {
-                        "type": "text",
-                        "analyzer": "asciifolded_people_analyzer",
-                    },
+                "properties": {
+                    "boost_10": {"type": "text", "boost": 10, **analyzer_fields},
+                    "boost_5": {"type": "text", "boost": 5, **analyzer_fields},
+                    "boost_1": {"type": "text", "boost": 1, **analyzer_fields}
                 }
-            },
+            }
         ),
-        PatchIndexPropertyMapping(
-            "metadata.contributors.person_or_org.name",
-            {
-                "fields": {
-                    "_search": {"type": "text", "analyzer": "people_analyzer"},
-                    "_ascii_search": {
-                        "type": "text",
-                        "analyzer": "asciifolded_people_analyzer",
-                    },
-                }
-            },
-        ),
+        *copy_to_mappings,
+        SetDefaultSearchFields("boost_10", "boost_5", "boost_1", "boost_10._search",
+                               "boost_5._search", "boost_1._search", "boost_10._ascii_search",
+                               "boost_5._ascii_search", "boost_1._ascii_search")
     ],
     configuration={"ui_blueprint_name": "datasets_ui"},
 )
