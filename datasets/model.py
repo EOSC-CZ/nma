c09a02cf947c1b86f0eb23b03bf00c965362d1dd
ron <kristron6@gmail.com>
2026-02-05 11:37:05 +0100

    savepoint

--- datasets/model.py
+++ datasets/model.py
@@ -116,28 +116,6 @@ class UpdatableRecordServiceMixin:
             identity, id_, data, *args, revision_id=revision_id, **kwargs
         )
 
-
-class OverriddenRouteResourceConfigMixin:
-    @property
-    def routes(self):
-        """Override routes to use path instead of default converter for pid_value.
-
-        This was causing a problem when PID contained slashes (doi:1234/zenodo.12345 for example).
-        It would parse only first part before the slash.
-        """
-        routes = super().routes
-
-        updated_routes = {}
-        for (
-            key,
-            route,
-        ) in routes.items():
-            updated_route = route.replace("<pid_value>", "<path:pid_value>")
-            updated_routes[key] = updated_route
-
-        return updated_routes
-
-
 COPY_TO_MAPPINGS = [
     # boost_10 - Primary identifiers (highest weight)
     ("metadata.title", 10),
@@ -243,7 +221,6 @@ datasets_model = model(
         PrependMixin("PIDFieldContext", ExternalPIDFieldContextMixin),
         PrependMixin("Draft", PIDStatusCheckFieldMixin),
         PrependMixin("RecordService", UpdatableRecordServiceMixin),
-        PrependMixin("RecordResourceConfig", OverriddenRouteResourceConfigMixin),
         AddFacetGroup(
             "default",
             [
