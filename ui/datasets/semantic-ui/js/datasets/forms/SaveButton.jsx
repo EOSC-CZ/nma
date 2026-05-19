0ce27a48aae0ffccba9ac4621b80cb9e97611681
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2026-05-19 13:28:26 +0200

    reset save button

--- ui/datasets/semantic-ui/js/datasets/forms/SaveButton.jsx
+++ ui/datasets/semantic-ui/js/datasets/forms/SaveButton.jsx
@@ -1,1 +1,18 @@
-export { SaveButton, SaveButton as default } from "@js/invenio_rdm_records";
+import { SaveButtonComponent } from "@js/oarepo_ui/forms/components/SaveButton/SaveButton";
+import { save } from "./actions";
+import { connect } from "react-redux";
+
+const mapDispatchToProps = (dispatch) => ({
+  saveAction: (values, params) => dispatch(save(values, params)),
+});
+
+const mapStateToProps = (state) => ({
+  actionState: state.deposit.actionState,
+});
+
+export const SaveButton = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SaveButtonComponent);
+
+export default SaveButton;
