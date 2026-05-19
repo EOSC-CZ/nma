64ad72340e47c90aa03d94a27fe493346398bcb4
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2026-05-19 12:24:26 +0200

    fix(openaire): update OAI-PMH harvester for new OARepo stack

--- ui/datasets/semantic-ui/js/datasets/forms/SaveButton.jsx
+++ ui/datasets/semantic-ui/js/datasets/forms/SaveButton.jsx
@@ -1,18 +1,1 @@
-import { SaveButtonComponent } from "@js/oarepo_ui/forms/components/SaveButton/SaveButton";
-import { save } from "./actions";
-import { connect } from "react-redux";
-
-const mapDispatchToProps = (dispatch) => ({
-  saveAction: (values, params) => dispatch(save(values, params)),
-});
-
-const mapStateToProps = (state) => ({
-  actionState: state.deposit.actionState,
-});
-
-export const SaveButton = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SaveButtonComponent);
-
-export default SaveButton;
+export { SaveButton, SaveButton as default } from "@js/invenio_rdm_records";
